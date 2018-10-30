508761015b5c887209ad6df20a0055d0fda1771d
Ryan Daley <rdaley321@yahoo.com>
2018-10-30 00:26:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(line, name) 
   line << name
-  puts "Welcome #{name}. You are number #{line.length} in line."
+  puts "Welcome, #{name}. You are number #{line.length} in line."
 end
